fe0f560e6ee74a8ac49f02b6a9b49b318bcb93d6
SpencerDawe <spencerdawe@cmail.carleton.ca>
2018-09-19 22:53:38 -0400

    Added test cases for aces turning to 1's or staying at 11

--- src/test/java/HandTest.java
+++ src/test/java/HandTest.java
@@ -48,4 +48,64 @@ public class HandTest extends TestCase{
 		decktest.deal(testhand);
 		assertEquals(14,testhand.stand());
 	}
+	
+	public void testAce11() {
+		Deck decktest = new Deck();
+		decktest.add("H3", 3);
+		decktest.add("DA",11);
+		Hand testhand = new Hand();
+		decktest.deal(testhand);
+		assertEquals(11,testhand.getCardValue(0));
+	}
+	
+	public void testAce1() {
+		Deck decktest = new Deck();
+		decktest.add("H3", 3);
+		decktest.add("DK",10);
+		decktest.add("DA",11);
+		Hand testhand = new Hand();
+		decktest.deal(testhand);
+		decktest.hit(testhand);
+		assertEquals(1,testhand.getCardValue(0));
+	}
+	
+	public void testAce11then1() {
+		Deck decktest = new Deck();
+		decktest.add("H3", 3);
+		decktest.add("DA",11);
+		decktest.add("DK",10);
+		Hand testhand = new Hand();
+		decktest.deal(testhand);
+		assertEquals(11,testhand.getCardValue(1));
+		decktest.hit(testhand);
+		assertEquals(1,testhand.getCardValue(1));
+	}
+	
+	public void testAcetwo1() {
+		Deck decktest = new Deck();
+		decktest.add("H3", 3);
+		decktest.add("DK",10);
+		decktest.add("DA",11);
+		decktest.add("HA",11);
+		Hand testhand = new Hand();
+		decktest.deal(testhand);
+		decktest.hit(testhand);
+		decktest.hit(testhand);
+		assertEquals(1,testhand.getCardValue(1));
+		assertEquals(1,testhand.getCardValue(0));
+	}
+	
+	public void testAceOne1One11() {
+		Deck decktest = new Deck();
+		decktest.add("H3", 3);
+		decktest.add("DA",11);
+		decktest.add("HA",11);
+		Hand testhand = new Hand();
+		decktest.deal(testhand);
+		decktest.hit(testhand);
+		assertEquals(11,testhand.getCardValue(1));
+		assertEquals(1,testhand.getCardValue(0));
+	}
+	
+	
 }
